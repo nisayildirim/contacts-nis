c6acb6926eccef9960124eb1d592e2417d8f82e9
Nisa Yıldırım <nisayildrm96@gmail.com>
2022-04-25 08:38:26 +0300

    trial

---
+++ src/components/Contacts/Form/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Form() {
+  return (
+    <div>
+      <div>
+        <input name="fullname" placeholder="Full Name" />
+      </div>
+      <div>
+        <input name="phone_number" placeholder="Phone Number" />
+      </div>
+    </div>
+  );
+}
+
+export default Form;
